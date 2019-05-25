48bf96f2b7f691cd740ca2ff5fdc4a024f0ded4d
Jairo Moreno <master.zion@gmail.com>
2019-05-25 12:59:22 +0200

    first commit

---
+++ README.txt
@@ -0,0 +1,5 @@
+A Pen created at CodePen.io. You can find this one at https://codepen.io/jnowland/pen/iexDp.
+
+ In an attempt to sharpen my pseudo element abuse I tried to recreate this:
+http://dribbble.com/shots/711443-Light-Dark?list=likes
+
---
+++ js/index.js
@@ -0,0 +1,91 @@
+var CNC_AXIS = {};///////////////////////////////////////////////////////////////////////////////
+
+CNC_AXIS['x']=0;
+CNC_AXIS['y']=0;
+CNC_AXIS['z']=0;
+
+
+//JS to make it work on single page/codepen
+var MainNav = $('.MainNav-Button');
+
+MainNav.on('mousedown', function(){
+	var $this = $(this);
+	$this.parent().find('.MainNav-Button').removeClass('MainNav-Button_LeftOfActive MainNav-Button_RightOfActive MainNav-Button_Active');
+	$this.addClass('MainNav-Button_Active').prev().addClass('MainNav-Button_LeftOfActive');
+	$this.next().addClass('MainNav-Button_RightOfActive');
+});
+
+MainNav.on('mouseup', function(){
+	var $this = $(this);
+	$this.parent().find('.MainNav-Button').removeClass('MainNav-Button_LeftOfActive MainNav-Button_RightOfActive MainNav-Button_Active');
+});
+
+MainNav.on('click', function(event){
+	event.preventDefault();
+});
+
+
+
+$(document).keydown(function(e) {
+    switch(e.which) {
+        case 37: // left
+            $("#y_down").mousedown();
+        break;
+
+        case 38: // up
+            CNC_AXIS['x']+=1;
+            $("#x_up").mousedown();
+        break;
+
+        case 39: // right
+            $("#y_up").mousedown();
+        break;
+
+        case 40: // down
+            $("#x_down").mousedown();
+        break;
+
+        case 33: // page up
+            $("#z_up").mousedown();
+        break;
+
+        case 34: // page down
+            $("#z_down").mousedown();
+        break;
+        default: return; // exit this handler for other keys
+    }
+    console.log(CNC_AXIS);
+    e.preventDefault(); // prevent the default action (scroll / move caret)
+});
+
+
+$(document).keyup(function(e) {
+    switch(e.which) {
+        case 37: // left
+            $("#y_down").mouseup();
+        break;
+
+        case 38: // up
+            $("#x_up").mouseup();
+        break;
+
+        case 39: // right
+            $("#y_up").mouseup();
+        break;
+
+        case 40: // down
+            $("#x_down").mouseup();
+        break;
+
+        case 33: // page up
+            $("#z_up").mouseup();
+        break;
+
+        case 34: // page down
+            $("#z_down").mouseup();
+        break;
+        default: return; // exit this handler for other keys
+    }
+    console.log(CNC_AXIS);
+    e.preventDefault(); // prevent the default action (scroll / move caret)
+});
